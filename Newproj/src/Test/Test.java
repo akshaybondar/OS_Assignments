367b4ba728680cd71d614ecf47ba0e3249b3d65d
aksha <aksha@LAPTOP-Q0CP1PAC>
2020-10-12 18:25:29 +0530

    AdvJava

---
+++ Newproj/src/Test/Test.java
@@ -0,0 +1,36 @@
+package Test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class Test extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		resp.setContentType("text/plain");
+		PrintWriter out = resp.getWriter();
+		out.print("Hello");
+		out.flush();
+		out.close();
+		
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		PrintWriter out = resp.getWriter();
+		out.print("Bye");
+		out.flush();
+		out.close();
+		
+	}
+
+}
